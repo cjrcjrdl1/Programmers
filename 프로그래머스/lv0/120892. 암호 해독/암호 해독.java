320ae82a284b7786521a4c477e74c6079c0d86f3
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-03-23 22:33:58 +0900

    [level 0] Title: 암호 해독, Time: 12.53 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        for(int i =1;i<=cipher.length()/code;i++){
+            answer += cipher.charAt(code*i-1);
+            
+        }
+        return answer;
+    }
+}
