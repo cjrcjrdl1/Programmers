de26a70879a4292c92f92fa7d758f9e3996c77ca
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-04-03 00:03:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.73 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 95.7 MB, 시간: 285.46 ms
+메모리: 95.4 MB, 시간: 68.73 ms
 
 ### 구분
 
--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,15 +1,42 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+//         String answer = "";
+//         Arrays.sort(participant);
+//         Arrays.sort(completion);
         
-        int i = 0;
-        for(;i<completion.length;i++){
-            if(!participant[i].equals(completion[i]))
-                break;
+//         int i = 0;
+//         for(;i<completion.length;i++){
+//             if(!participant[i].equals(completion[i]))
+//                 break;
+//         }
+//         return participant[i];
+        String answer = "";
+        HashMap<String,Integer> map  = new HashMap<>();
+        for(String player : participant){
+            map.put(player, map.getOrDefault(player,0)+1);
         }
-        return participant[i];
+        
+        for(String player : completion){
+            map.put(player, map.get(player)-1);
+        }
+        
+        // for(String key : map.keySet()){
+        //     if(map.get(key)!=0){
+        //         answer = key;
+        //         break;
+        //     }
+        // }
+        Iterator<Map.Entry<String,Integer>> iter = map.entrySet().iterator();
+        while(iter.hasNext()){
+            Map.Entry<String,Integer> entry = iter.next();
+            if(entry.getValue()!=0){
+                answer = entry.getKey();
+                break;
+            }
+        }
+        return answer;
     }
 }
