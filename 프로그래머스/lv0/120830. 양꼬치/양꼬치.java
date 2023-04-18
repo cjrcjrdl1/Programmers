5519b7f41093c2b66f283f8663ba601b6d1a6441
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-04-18 12:01:57 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        answer = (12000*n) + (2000*k);
+        if(n>=10){
+            answer -= 2000*(n/10);
+        }
+        return answer;
+    }
+}
