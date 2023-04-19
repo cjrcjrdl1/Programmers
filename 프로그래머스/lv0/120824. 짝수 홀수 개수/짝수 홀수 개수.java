0c9ef16d5864323b6f95d3fe4c18da5e9804d4d0
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-04-19 10:53:57 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 1.29 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+
+        answer[0] = (int)Arrays.stream(num_list).filter(i->i%2==0).count();
+        answer[1] = (int)Arrays.stream(num_list).filter(i->i%2==1).count();
+        return answer;
+    }
+}
