ea741f868451c5127be74a7075725e2906778465
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-04-30 12:58:39 +0900

    [Silver I] Title: 절댓값 힙, Time: 612 ms, Memory: 27088 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,33 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        PriorityQueue<Integer> myQueue = new PriorityQueue<>((o1, o2) -> {
+            int first_abs = Math.abs(o1);
+            int second_abs = Math.abs(o2);
+            if (first_abs == second_abs) { //절대값이 같은 경우 음수 우선
+                return o1 > o2 ? 1 : -1;
+            }
+            return first_abs - second_abs; //절대값이 작은 데이터 우선
+        });
+
+        for (int i = 0; i < N; i++) {
+            int request = Integer.parseInt(bf.readLine());
+            if (request == 0) {
+                if (myQueue.isEmpty()) {
+                    System.out.println("0");
+                } else {
+                    System.out.println(myQueue.poll());
+                }
+            } else {
+                myQueue.add(request);
+            }
+        }
+    }
+}
