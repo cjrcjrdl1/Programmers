824bf176917821cfba2fb208843a134d56353fe3
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-05-10 20:44:20 +0900

    [Silver III] Title: 칸토어 집합, Time: 352 ms, Memory: 19860 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -0,0 +1,46 @@
+
+import java.io.*;
+
+public class Main {
+
+    static int N;
+    static char c[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str;
+        while ((str = br.readLine()) != null) {
+            N = Integer.parseInt(str);
+
+            int cnt = (int) Math.pow(3, N);
+            c = new char[cnt];
+
+            for (int i = 0; i < cnt; i++) {
+                c[i] = '-';
+            }
+
+            dfs(0, cnt);
+
+            for (int i = 0; i < cnt; i++) {
+                bw.write(c[i]);
+            }
+            bw.write('\n');
+            bw.flush();
+        }
+    }
+
+    static void dfs(int start, int length) {
+        if (length < 3) {
+            return;
+        }
+        for (int i = start + length / 3; i < start + length / 3 * 2; i++) {
+            c[i] = ' ';
+        }
+
+        dfs(start, length/3);
+        dfs(start+length/3*2, length/3);
+    }
+
+
+}
