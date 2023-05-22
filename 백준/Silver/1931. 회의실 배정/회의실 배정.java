2a54153f1c28b9c1dfacbd3f4fdfcb1e9857cbe7
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-05-22 15:03:33 +0900

    [Silver I] Title: 회의실 배정, Time: 532 ms, Memory: 43220 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,51 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        //time[][0] 시작시점
+        //time[][1] 종료시점
+
+        int[][] time = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(time, new Comparator<int[]>(){
+
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+        });
+
+        int count = 0;
+        int prev_end_time = 0;
+
+        for (int i = 0; i < N; i++) {
+            //직전 종료시간이 다음 회의 시간보다 작거나 가탇면 갱신
+            if (prev_end_time <= time[i][0]) {
+                prev_end_time = time[i][1];
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
