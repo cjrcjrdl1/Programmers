56f6d067537adc9b4fb320699f0b09527f268436
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-06-26 15:35:11 +0900

    [Gold V] Title: 토마토, Time: 628 ms, Memory: 120192 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -0,0 +1,81 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main{
+
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int n,m;
+    static int[][] map;
+    static Queue<int[]> q = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 1) {
+                    q.add(new int[]{i, j});
+                }
+            }
+        }
+
+        System.out.println(bfs());
+    }
+
+    private static int bfs() {
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            int nowX = now[0];
+            int nowY = now[1];
+            for (int i = 0; i < 4; i++) {
+                int nx = nowX + dx[i];
+                int ny = nowY + dy[i];
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m && map[nx][ny] == 0) {
+                    map[nx][ny] = map[nowX][nowY] + 1;
+                    q.add(new int[]{nx, ny});
+                }
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        if (checkZero()) {
+            return -1;
+        } else {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (max < map[i][j]) {
+                        max = map[i][j];
+                    }
+                }
+            }
+            return max - 1;
+        }
+    }
+
+    private static boolean checkZero() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 0) {
+                    return true;
+                }
+            }
+        }
+            return false;
+    }
+
+
+}
