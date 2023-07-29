5c2f7dad8348025df3e14866968244464066d659
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-07-29 09:44:53 +0900

    [Bronze IV] Title: 숫자의 합, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -1,19 +1,21 @@
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        String str = sc.next();
-        char[] s = str.toCharArray();
 
-        int sum = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        char[] chars = br.readLine().toCharArray();
+
+        int answer = 0;
+
         for (int i = 0; i < N; i++) {
-            sum += s[i] - '0';
+            answer += chars[i] - '0';
         }
 
-        System.out.println(sum);
+        System.out.println(answer);
     }
 }
