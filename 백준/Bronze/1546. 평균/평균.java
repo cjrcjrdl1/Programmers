a97cba0d03b07b8b848a29065a9a010cca59d718
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-07-29 09:59:45 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,22 +1,24 @@
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] A = new int[N];
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
+            A[i] = Integer.parseInt(st.nextToken());
         }
 
-        int max = 0;
         int sum = 0;
-
+        int max = 0;
         for (int i = 0; i < N; i++) {
-            if (max < A[i]) {
+            if (A[i] > max) {
                 max = A[i];
             }
             sum += A[i];
