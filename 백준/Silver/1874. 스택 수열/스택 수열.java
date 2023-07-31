e72062b394a7010aea8a8d9fc1be766b45264dd4
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-07-31 10:54:52 +0900

    [Silver II] Title: 스택 수열, Time: 352 ms, Memory: 26136 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,20 +1,25 @@
+
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int A[] = new int[N];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
         for (int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
+            A[i] = Integer.parseInt(br.readLine());
         }
+
         Stack<Integer> stack = new Stack<>();
+        StringBuffer bf = new StringBuffer();
         int num = 1;
         boolean result = true;
-        StringBuffer bf = new StringBuffer();
-        for (int i = 0; i<A.length; i++) {
+        for (int i = 0; i < A.length; i++) {
             int su = A[i];
             if (su >= num) {
                 while (su >= num) {
@@ -25,6 +30,7 @@ public class Main {
                 bf.append("-\n");
             } else {
                 int n = stack.pop();
+
                 if (n > su) {
                     System.out.println("NO");
                     result = false;
@@ -34,8 +40,8 @@ public class Main {
                 }
             }
         }
-        if (result) {
-            System.out.println(bf.toString());
-        }
+        if (result) System.out.println(bf.toString());
+        
     }
 }
+
