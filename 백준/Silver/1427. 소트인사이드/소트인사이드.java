5e221506f21fcdf03d5a1b3acf431f320d100b59
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-08-03 10:19:01 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,22 +1,24 @@
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        int A[] = new int[str.length()];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int[] A = new int[str.length()];
         for (int i = 0; i < str.length(); i++) {
-            A[i] = Integer.parseInt(str.substring(i,i+1));
+            A[i] = Integer.parseInt(str.substring(i, i + 1));
         }
 
         for (int i = 0; i < str.length(); i++) {
             int max = i;
-            for (int j = i + 1; j < str.length(); j++) {
+            for (int j = i+1; j < str.length(); j++) {
                 if (A[j] > A[max]) {
                     max = j;
                 }
             }
-
             if (A[i] < A[max]) {
                 int temp = A[i];
                 A[i] = A[max];
@@ -27,5 +29,6 @@ public class Main {
         for (int i = 0; i < str.length(); i++) {
             System.out.print(A[i]);
         }
+
     }
 }
