b870fe42b55dae8abd0b46bcb4402758178c357c
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-08-04 13:43:49 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1136 ms, Memory: 120348 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,27 +1,63 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.*;
 
 public class Main {
-
+    public static int[] A, tmp;
+    public static long result;
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        A = new int[N + 1];
+        tmp = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+        merge_sort(1, N);
+        for (int i = 1; i <= N; i++) {
+            bw.write(A[i] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
 
-        int N = Integer.parseInt(bf.readLine());
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            list.add(Integer.parseInt(bf.readLine()));
+    public static void merge_sort(int s, int e) {
+        if (e - s < 1) {
+            return;
+        }
+        int m = s + (e - s) / 2;
+        //재귀 함수 형태로 구현
+        merge_sort(s, m);
+        merge_sort(m + 1, e);
+        for (int i = s; i <= e; i++) {
+            tmp[i] = A[i];
+        }
+        int k = s;
+        int index1 = s;
+        int index2 = m + 1;
+        while (index1 <= m && index2 <= e) { //두 그룹을 병합하는 로직
+            //양쪽 그룹의 index가 가리키는 값을 비교해 더 작은 수를 선택해 배열에 저장하고
+            //선택된 데이터의 index 값을 오른쪽으로 한 칸 이동하기
+            if (tmp[index1] > tmp[index2]) {
+                A[k] = tmp[index2];
+                k++;
+                index2++;
+            } else {
+                A[k] = tmp[index1];
+                k++;
+                index1++;
+            }
+        }
+        while (index1 <= m) { //한쪽 그룹이 모두 선택된 후 남아 있는 값 정리하기
+            A[k] = tmp[index1];
+            k++;
+            index1++;
+        }
+        while (index2 <= e) {
+            A[k] = tmp[index2];
+            k++;
+            index2++;
         }
 
-        Collections.sort(list);
-
-        for (int i : list) {
-            sb.append(i).append("\n");
-        }
-        System.out.println(sb);
     }
 }
