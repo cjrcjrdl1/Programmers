37f121657c4af7e3edfcc76cc24087607c078130
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-08-15 09:22:20 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,39 +2,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int n, count;
     static int[][] arr;
-    static boolean check[];
-    static int n, m, answer = 0;
-    public static void DFS(int start) {
-        check[start] = true;
-        answer++;
-
-        for (int i = 1; i <= n; i++) {
-            if (arr[start][i] == 1 && !check[i]) {
-                DFS(i);
-            }
-        }
-    }
-
+    static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        arr = new int[n+1][n+1];
+        arr = new int[n + 1][n + 1];
+        visited = new boolean[n + 1];
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            arr[a][b] = arr[b][a] = 1;
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arr[x][y] = arr[y][x] = 1;
         }
-        check = new boolean[n + 1];
-        DFS(1);
 
-        System.out.println(answer-1);
+        dfs(1);
+
+        System.out.println(count);
     }
 
+    static void dfs(int start) {
+        visited[start] = true;
+
+        for (int i = 1; i < n + 1; i++) {
+            if (visited[i] == false && arr[start][i] == 1) {
+                count++;
+                dfs(i);
+            }
+        }
+    }
 }
