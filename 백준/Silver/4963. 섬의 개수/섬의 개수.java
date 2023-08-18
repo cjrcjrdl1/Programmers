b987940f532860ecc2f86c1b72ab016318695c7e
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-08-18 10:52:54 +0900

    [Silver II] Title: 섬의 개수, Time: 160 ms, Memory: 15964 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -0,0 +1,57 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int w,h;
+    static int[][] arr;
+    static boolean[][] visited;
+    static int dx[] = {0, 0, -1, 1, -1, 1, -1, 1};
+    static int dy[] = {-1, 1, 0, 0, 1, 1, -1, -1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+            if (w == 0 && h == 0) {
+                break;
+            }
+            arr = new int[h][w];
+            visited = new boolean[h][w];
+
+            for (int i = 0; i < h; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < w; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            int count = 0;
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (!visited[i][j] && arr[i][j] == 1) {
+                        count++;
+                        dfs(i, j);
+                    }
+                }
+            }
+            System.out.println(count);
+        }
+    }
+
+    static void dfs(int x, int y) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 8; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && ny >= 0 && nx < h && ny < w && !visited[nx][ny] && arr[nx][ny] == 1) {
+                dfs(nx, ny);
+            }
+        }
+    }
+}
