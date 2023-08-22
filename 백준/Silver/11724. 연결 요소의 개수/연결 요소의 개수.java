e7e1e8777a83086c5e0e4cb93306dd8c437c9943
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-08-22 16:06:07 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 560 ms, Memory: 116016 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,57 +2,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main{
-
+public class Main {
     static int n,m;
-    static int[][] map;
+    static int arr[][];
     static boolean visited[];
-    static int count=0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        map = new int[n+1][n+1];
+
+        arr = new int[n + 1][n + 1];
         visited = new boolean[n + 1];
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            map[x][y] = map[y][x] = 1;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[a][b] = arr[b][a] = 1;
         }
 
-        count = 0;
-        for (int i = 1; i <= n; i++) {
-            if (visited[i] == false) {
+        int count = 0;
+        for (int i = 1; i < n + 1; i++) {
+            if (!visited[i]) {
                 dfs(i);
                 count++;
             }
         }
 
         System.out.println(count);
-
     }
 
-    public static void dfs(int start) {
-
-        if (visited[start] == true) {
-            return;
-        }
-
+    private static void dfs(int start) {
         visited[start] = true;
-        for (int i = 1; i <= n; i++) {
-            if (map[start][i] == 1) {
+
+        for (int i = 1; i < n + 1; i++) {
+            if (arr[start][i] == 1 && !visited[i]) {
                 dfs(i);
             }
         }
     }
-
-
 
 
 }
