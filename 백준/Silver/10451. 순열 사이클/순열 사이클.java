48e0d7e8b6359f96b984b3f2a52f44ec9e60df7e
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-08-22 20:17:21 +0900

    [Silver III] Title: 순열 사이클, Time: 540 ms, Memory: 52204 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.java
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.java
@@ -5,46 +5,38 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int cycle;
     static int[] arr;
     static boolean[] visited;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int T = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < T; i++) {
+        for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
             arr = new int[n + 1];
-            cycle = 0;
-
-            st = new StringTokenizer(br.readLine());
+            visited = new boolean[n + 1];
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 1; j < n + 1; j++) {
                 arr[j] = Integer.parseInt(st.nextToken());
             }
 
-            visited = new boolean[n + 1];
-
+            int count = 0;
             for (int j = 1; j < n + 1; j++) {
                 if (!visited[j]) {
-                    DFS(j);
-                    cycle++;
+                    dfs(j);
+                    count++;
                 }
             }
-            System.out.println(cycle);
-
+            System.out.println(count);
         }
-
     }
 
-    public static void DFS(int start) {
-
+    static void dfs(int start) {
         visited[start] = true;
-
         int next = arr[start];
+
         if (!visited[next]) {
-            DFS(arr[start]);
+            dfs(next);
         }
     }
 }
