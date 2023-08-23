e58d4751b13c4ea690e115d66792c36c173d5ccc
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-08-23 17:52:14 +0900

    [Silver I] Title: 트리 순회, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -2,19 +2,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static Node head = new Node('A',null,null);
+    static Node head = new Node('A', null, null);
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-
             char root = st.nextToken().charAt(0);
             char left = st.nextToken().charAt(0);
             char right = st.nextToken().charAt(0);
@@ -29,32 +25,31 @@ public class Main {
         postOrder(head);
     }
 
-    public static void preOrder(Node node) {
-        if (node==null) return;
+    private static void preOrder(Node node) {
+        if (node == null) return;
         System.out.print(node.value);
         preOrder(node.left);
         preOrder(node.right);
     }
 
-    public static void inOrder(Node node) {
-        if (node==null) return;
+    private static void inOrder(Node node) {
+        if (node == null) return;
         inOrder(node.left);
         System.out.print(node.value);
         inOrder(node.right);
-
     }
 
-    public static void postOrder(Node node) {
-        if (node==null) return;
+    private static void postOrder(Node node) {
+        if (node == null) return;
         postOrder(node.left);
         postOrder(node.right);
         System.out.print(node.value);
     }
 
-    public static void insertNode(Node temp, char root, char left, char right) {
+    private static void insertNode(Node temp, char root, char left, char right) {
         if (temp.value == root) {
-            temp.left = (left == '.' ? null : new Node(left, null, null));
-            temp.right = (right == '.' ? null : new Node(right, null, null));
+            temp.left = (left == '.') ? null : new Node(left, null, null);
+            temp.right = (right == '.') ? null : new Node(right, null, null);
         } else {
             if (temp.left!=null) insertNode(temp.left, root, left, right);
             if (temp.right!=null) insertNode(temp.right, root, left, right);
@@ -66,11 +61,12 @@ public class Main {
         Node left;
         Node right;
 
-        Node(char value, Node left, Node right) {
+        public Node(char value, Node left, Node right) {
             this.value = value;
             this.left = left;
             this.right = right;
         }
     }
+
 
 }
