4b507e2f153c5a2f8e9e67761f0051ad08a40c9d
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-08-26 09:00:46 +0900

    [Silver I] Title: 미로 탐색, Time: 132 ms, Memory: 14588 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,61 +2,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-
-    static int[][] map;
-    static int n,m;
+    static int n, m;
+    static int[][] arr;
     static boolean[][] visited;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        map = new int[n][m];
+        arr = new int[n][m];
+        visited = new boolean[n][m];
+
         for (int i = 0; i < n; i++) {
             String s = br.readLine();
             for (int j = 0; j < m; j++) {
-                map[i][j] = s.charAt(j) - '0';
+                arr[i][j] = s.charAt(j) - '0';
             }
         }
-        visited = new boolean[n][m];
-        visited[0][0] = true;
-        BFS(0,0);
-        System.out.println(map[n-1][m-1]);
 
+        bfs(0, 0);
+
+        System.out.println(arr[n-1][m-1]);
     }
 
-    public static void BFS(int x, int y) {
+    private static void bfs(int x, int y) {
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{x, y});
+        visited[x][y] = true;
 
         while (!q.isEmpty()) {
             int[] now = q.poll();
-            int nowX = now[0];
-            int nowY = now[1];
-
             for (int i = 0; i < 4; i++) {
-                int nextX = nowX + dx[i];
-                int nextY = nowY + dy[i];
-
-                if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) {
-                    continue;
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m && !visited[nx][ny] && arr[nx][ny] == 1) {
+                    arr[nx][ny] = arr[now[0]][now[1]] + 1;
+                    visited[nx][ny] = true;
+                    q.add(new int[]{nx, ny});
                 }
-                if (visited[nextX][nextY] || map[nextX][nextY] == 0) {
-                    continue;
-                }
-
-                q.add(new int[]{nextX, nextY});
-                map[nextX][nextY] = map[nowX][nowY] + 1;
-                visited[nextX][nextY] = true;
             }
         }
     }
