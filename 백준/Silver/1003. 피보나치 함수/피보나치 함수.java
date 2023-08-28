463985b9416a2fe13df9d3ccb4e24e8c9ec62d58
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-08-28 20:00:35 +0900

    [Silver III] Title: 피보나치 함수, Time: 124 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -5,14 +5,11 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static Integer[][] dp = new Integer[41][2];
+    static int zero;
+    static int one;
+    static int zero_plus_one;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        dp[0][0] = 1;
-        dp[0][1] = 0;
-        dp[1][0] = 0;
-        dp[1][1] = 1;
 
         int t = Integer.parseInt(br.readLine());
 
@@ -21,20 +18,23 @@ public class Main {
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
             fibonacci(n);
-            sb.append(dp[n][0] + " " + dp[n][1]).append('\n');
+            sb.append(zero).append(' ').append(one).append('\n');
         }
-
 
         System.out.println(sb);
     }
 
-    static Integer[] fibonacci(int n) {
-        if (dp[n][0] == null || dp[n][1] == null) { //아직 탐색하지 않은 곳 null
-            dp[n][0] = fibonacci(n - 1)[0] + fibonacci(n - 2)[0];
-            dp[n][1] = fibonacci(n - 1)[1] + fibonacci(n - 2)[1];
-        }
+    static void fibonacci(int n) {
+        //초기값 세팅
+        zero = 1;
+        one = 0;
+        zero_plus_one = 1;
 
-        return dp[n];
+        for (int i = 0; i < n; i++) {
+            zero = one;
+            one = zero_plus_one;
+            zero_plus_one = zero + one;
+        }
     }
 
 }
