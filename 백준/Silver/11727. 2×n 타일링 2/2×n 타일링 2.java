5b6bfa64224524518969fb44f3277040a6c23734
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-09-12 10:35:28 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -4,25 +4,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static Long[] dp = new Long[1001];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        dp[0] = 0L;
-        dp[1] = 1L;
-        dp[2] = 3L;
+        long[] dp = new long[1001];
+        dp[1] = 1;
+        dp[2] = 3;
 
-
-        System.out.println(logic(n));
-    }
-
-    static long logic(int n) {
-        if (dp[n] == null) {
-            dp[n] = (logic(n - 2) * 2 + logic(n - 1)) % 10007;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10007;
         }
 
-        return dp[n];
+        System.out.println(dp[n]);
     }
 
 }
