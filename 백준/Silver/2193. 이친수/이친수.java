3a0cc4fc9a18260cdaf8d6fdbb928f7f4b947543
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-09-15 20:52:32 +0900

    [Silver III] Title: 이친수, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -4,21 +4,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    static Long[] dp = new Long[91];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        dp[0] = 0L;
+        dp[1] = 1L;
+
         int n = Integer.parseInt(br.readLine());
-        long[] dp = new long[91];
 
-        dp[0] = 0;
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 2] + dp[i - 1];
-        }
+        System.out.println(logic(n));
 
-        System.out.println(dp[n]);
     }
 
-
+    static long logic(int n) {
+        if (dp[n] == null) {
+            dp[n] = logic(n - 2) + logic(n - 1);
+        }
+        return dp[n];
+    }
 
 }
