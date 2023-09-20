649c2f47689f3dafb4bec506053971185b1cd9e0
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-09-20 20:19:13 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -4,42 +4,39 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static Long[][] dp;
-    static int n;
-    final static long mod = 1000000000;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        n = Integer.parseInt(br.readLine());
-        dp = new Long[n + 1][10];
+        long dp[][] = new long[n + 1][10];
+        long mod = 1000000000;
 
-        for (int i = 0; i < 10; i++) {
-            dp[1][i] = 1L;
+        for (int i = 1; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) { //1번째 자리수는 탐색할필요 X
+            for (int j = 0; j < 10; j++) {
+                if (j == 0) {
+                    dp[i][0] = dp[i - 1][1] % mod;
+                } else if (j == 9) {
+                    dp[i][9] = dp[i - 1][8] % mod;
+                } else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % mod;
+                }
+            }
         }
 
         long result = 0;
 
-        for (int i = 1; i <= 9; i++) {
-            result += recur(n, i);
+        for (int i = 0; i < 10; i++) {
+            result += dp[n][i];
         }
+
+
         System.out.println(result % mod);
-    }
 
-    static long recur(int digit, int val) {//digit는 자릿수, val은 자릿값
-        if (digit == 1) {
-            return dp[digit][val];
-        }
-
-        if (dp[digit][val] == null) {
-            if (val == 0) {
-                dp[digit][val] = recur(digit - 1, 1); //val이 0인 경우 이전자리는 1밖에 못옴
-            } else if (val == 9) {
-                dp[digit][val] = recur(digit - 1, 8); //val이 9인 경우 이전자리는 8밖에 없음
-            } else {
-                dp[digit][val] = recur(digit - 1, val - 1) + recur(digit - 1, val + 1);
-            }
-        }
-        return dp[digit][val] % mod;
     }
 
 
